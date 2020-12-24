6f759bd03742eff986f61280a9135e3300a79537
Monwar Adeeb <monwar_adeeb@yahoo.com>
2020-12-24 11:23:29 +0600

    Worked on Checkin

--- checkin.py
+++ checkin.py
@@ -51,3 +51,11 @@ General = (26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
 Joint_Room = (46, 47, 48, 49, 50, 46, 47, 48, 49, 50)
 m = [9]
 G = []
+
+
+def restart_program():
+    """Restarts the current program.
+    Note: this function does not return. Any cleanup action (like
+    saving data) must be done before calling this function."""
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
