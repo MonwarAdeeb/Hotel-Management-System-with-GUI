aac4280871b7fe4cc513e0e8c8d6586c66fc2990
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-01-07 11:35:18 +0600

    Worked on CheckOut

--- checkout.py
+++ checkout.py
@@ -5,3 +5,16 @@ import pickle
 details_list = []
 l2 = []
 G = []
+
+
+def file_save():
+    NAME_PRO = details_list[0]
+    ADDRESS_PRO = details_list[1]
+    MOBILE_NO_PRO = details_list[2]
+    ROOM_NO_PRO = details_list[3]
+    PRICE_PRO = details_list[4]
+    f = open("hotel.dat", "ab")
+    a = save(NAME_PRO, ADDRESS_PRO, MOBILE_NO_PRO, ROOM_NO_PRO, PRICE_PRO)
+    pickle.dump(a, f, protocol=2)
+    f.close()
+    restart_program()
