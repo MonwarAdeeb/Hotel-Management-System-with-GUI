50221f26cd726969567165bf4d5b35d8f879e6b2
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-01-18 10:46:00 +0600

    Worked on GetInfo

--- getinfo.py
+++ getinfo.py
@@ -18,3 +18,11 @@ def file_save():
     pickle.dump(a, f, protocol=2)
     f.close()
     restart_program()
+
+
+def restart_program():
+    """Restarts the current program.
+    Note: this function does not return. Any cleanup action (like
+    saving data) must be done before calling this function."""
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
